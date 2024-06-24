8ba430aa73179f8714a11ce32d2b6406220b340e
MaciejBabicki <macbabicki@gmail.com>
2024-06-24 23:40:33 +0200

    Update CampaignService change an endpoint

--- src/service/CampaignService.js
+++ src/service/CampaignService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const REST_API_URL = "http://localhost:8080/campaign";
+const REST_API_URL = "http://localhost:8080/campaigns";
 
 export const listCampaigns = () => axios.get(REST_API_URL);
 
